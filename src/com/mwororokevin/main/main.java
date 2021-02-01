1a641c2016f73cfd69454bb8dc71d9be95c35d9a
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-02-01 21:26:54 +0300

    Add the main package with the main class.

---
+++ src/com/mwororokevin/main/main.java
@@ -0,0 +1,24 @@
+package com.mwororokevin.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Kevin Kamau Mwororo
+ * @author website mwororokevin.com
+ * @author email mwororokevin@gmail.com
+ */
+
+public class main extends Application {
+
+    
+
+    @Override
+    public void start(Stage stage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("/com/mwororokevin/application/Application.fxml"));
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

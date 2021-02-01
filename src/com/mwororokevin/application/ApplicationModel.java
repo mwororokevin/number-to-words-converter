cdb2f2ecce023544381cf4aeea7387a13a18ba68
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-02-01 22:48:01 +0300

    Initial Commit of the ApplicationModel class.

---
+++ src/com/mwororokevin/application/ApplicationModel.java
@@ -0,0 +1,14 @@
+package com.mwororokevin.application;
+
+/**
+ * @author Kevin Kamau Mwororo
+ * @author website mwororokevin.com
+ * @author email mwororokevin@gmail.com
+ */
+
+public class ApplicationModel {
+    
+    
+}
+
+

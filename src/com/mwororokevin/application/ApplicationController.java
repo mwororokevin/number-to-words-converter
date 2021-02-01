39b0abde17f91d79d7fcd20debc2f9f0e82542ba
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-02-01 22:13:09 +0300

    Add the FXIDs and the ActionEvent Methods for the FXML file.
    This includes the ids for the textarea, label and textfield. The Methods include convertNumber, copyText and clearText.

--- src/com/mwororokevin/application/ApplicationController.java
+++ src/com/mwororokevin/application/ApplicationController.java
@@ -1,8 +1,13 @@
 package com.mwororokevin.application;
 
+import java.awt.TextArea;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * @author Kevin Kamau Mwororo
@@ -11,7 +16,31 @@ import javafx.fxml.Initializable;
  */
 
 public class ApplicationController implements Initializable {
+    
+    @FXML
+    private TextField inputTxtField;
 
+    @FXML
+    private Label warningFeedbackLbl;
+
+    @FXML
+    private TextArea outputTextArea;
+
+    @FXML
+    private void clearText(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void convertNumber(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void copyText(ActionEvent event) {
+
+    }
+    
     /**
      * Initializes the controller class.
      */

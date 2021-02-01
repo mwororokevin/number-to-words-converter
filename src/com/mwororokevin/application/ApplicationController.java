281879a5838626b3fe44f302d46bfb1dd621f930
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-02-01 22:25:50 +0300

    Correct import for TextArea.
    It will change the import from awt to the correct one, scene.control.

--- src/com/mwororokevin/application/ApplicationController.java
+++ src/com/mwororokevin/application/ApplicationController.java
@@ -1,12 +1,12 @@
 package com.mwororokevin.application;
 
-import java.awt.TextArea;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 /**

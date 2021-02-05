097a1305a7312b3b352d2970bffe6804eba38667
Kevin Kamau Mwororo <mwororokevin@gmail.com>
2021-02-05 13:55:03 +0300

    Add the Stage Icon image the task bar.
    Initializing the stage icon image as stageIcon Image variable and setting it as the stage icon.

--- src/com/mwororokevin/main/main.java
+++ src/com/mwororokevin/main/main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -17,10 +18,16 @@ public class main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/com/mwororokevin/application/Application.fxml"));
-
         
+//        Initialize the stage icon image as stageIcon
+        Image stageIcon = new Image("/com/mwororokevin/resources/appIcon.png");
+        
+        /**
+         * Setting the Scene, the Stage Icon, the title, the boolean value for 
+         * resizing the stage.
+         */
         stage.setScene(new Scene(root));
-        
+        stage.getIcons().add(stageIcon);
         stage.setTitle("Number to Words Converter");
         stage.setResizable(false);
         stage.show();
